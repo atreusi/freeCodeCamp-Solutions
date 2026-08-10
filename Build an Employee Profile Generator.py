ce3761b160db45fd198923dcab6f2faceadd1f36
ismail <145690228+atreusi@users.noreply.github.com>
2026-08-10 18:24:18 +0200

    Update Build an Employee Profile Generator.py

--- Build an Employee Profile Generator.py
+++ Build an Employee Profile Generator.py
@@ -5,19 +5,14 @@ last_name = 'Doe'
 full_name = first_name + ' ' + last_name
 
 address = '123 Main Street'
-
 address += ', Apartment 4B'
 
 employee_age = 28
-
 employee_info = full_name + ' is ' + str(employee_age) + ' years old'
-
 print(employee_info)
 
 experience_years = 5
-
 experience_info = 'Experience: ' + str(experience_years) + ' years'
-
 print(experience_info)
 
 position = 'Data Analyst'
@@ -29,14 +24,15 @@ employee_card = f'Employee: {full_name} | Age: {employee_age} | Position: {posit
 print(employee_card)
 
 employee_code = 'DEV-2026-JD-001'
-department = employee_code[0:3]
 
+department = employee_code[0:3]
 print(department)
 
 year_code = employee_code[4:8]
-
 print(year_code)
 
 initials = employee_code[9:11]
-
 print(initials)
+
+last_three = employee_code[-3:]
+print(last_three)
